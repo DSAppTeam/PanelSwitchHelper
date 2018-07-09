af868ca8dcebc189c889321eaf3f9e67decb6892
yummyLau <yummyl.lau@gmail.com>
2018-07-09 21:08:26 +0800

    init project

---
+++ app/src/main/java/com/effective/android/panelswitchhelper/SampleActivity.java
@@ -0,0 +1,13 @@
+package com.effective.android.panelswitchhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+}
