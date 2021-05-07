d4499e5806847d8e7dbb03563fd168d9c48db5ff
yummylau <yummyl.lau@gmail.com>
2021-05-07 10:57:54 +0800

    #0优化demo recyclerview动态隐藏面板场景

--- app-androidx/src/main/java/com/example/demo/scene/chat/view/AutoHidePanelRecyclerView.java
+++ app-androidx/src/main/java/com/example/demo/scene/chat/view/AutoHidePanelRecyclerView.java
@@ -29,61 +29,17 @@ public class AutoHidePanelRecyclerView extends RecyclerView {
         this(context, attrs, 0);
     }
 
+
     public AutoHidePanelRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         setLayoutManager(new LinearLayoutManager(context));
-        addOnItemTouchListener(new OnItemTouchListener() {
+    }
 
-            public boolean isTouchInView(View view, MotionEvent event) {
-                if (view == null) {
-                    return false;
-                }
-                int[] location = new int[2];
-                view.getLocationOnScreen(location);
-                int x = location[0];
-                int y = location[1];
-                return x < event.getRawX() && event.getRawX() < x + view.getWidth()
-                        && y < event.getRawY() && event.getRawY() < y + view.getHeight();
-            }
-
-            public void hidePanel() {
-                if (panelSwitchHelper != null) {
-                    panelSwitchHelper.hookSystemBackByPanelSwitcher();
-                }
-            }
-
-            @Override
-            public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
-                int action = e.getAction();
-                if (action == MotionEvent.ACTION_DOWN) {
-                    View view = rv.findChildViewUnder(e.getX(), e.getY());
-                    if (view == null) {
-                        hidePanel();
-                        return false;
-                    }
-                    View text = view.findViewById(R.id.text);
-                    View avatar = view.findViewById(R.id.avatar);
-                    if (text == null || avatar == null) {
-                        hidePanel();
-                        return false;
-                    }
-
-                    if (!isTouchInView(text, e) && !isTouchInView(avatar, e)) {
-                        hidePanel();
-                    }
-                }
-                return false;
-            }
-
-            @Override
-            public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
-
-            }
-
-            @Override
-            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-
-            }
-        });
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        if(panelSwitchHelper != null){
+            panelSwitchHelper.hookSystemBackByPanelSwitcher();
+        }
+        return super.onTouchEvent(e);
     }
 }
