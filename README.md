94b62d97a20119bc8e4b35e1c7c89b51f3d99cff
dodge <z.hero.dodge@gmail.com>
2025-08-04 12:02:55 +0800

    优化导航栏可见性判断，测试发现部分 Android 11以下手机通过WindowInsetsCompat判断不准确的问题

--- README.md
+++ README.md
@@ -56,7 +56,7 @@ allprojects {
 ```groovy
 //1.4.0 版本及后续，仅支持 Androidx
 dependencies {
-    implementation 'com.github.DSAppTeam:PanelSwitchHelper:v1.5.13'
+    implementation 'com.github.DSAppTeam:PanelSwitchHelper:v1.5.14'
 }
 ```
 
--- panel-androidx/src/main/java/com/effective/android/panel/utils/DisplayUtil.kt
+++ panel-androidx/src/main/java/com/effective/android/panel/utils/DisplayUtil.kt
@@ -184,16 +184,15 @@ object DisplayUtil {
     @JvmStatic
     @TargetApi(14)
     fun isNavigationBarShow(context: Context, window: Window): Boolean {
-        // 添加 windowInsetsCompat 判断是否含有导航栏
+        // 添加 WindowInsets 判断是否含有导航栏
         return isNavBarVisibleByInsets(window) || isNavBarVisible(context, window)
     }
 
 
     private fun isNavBarVisibleByInsets(window: Window): Boolean {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             val insets = window.decorView.rootWindowInsets ?: return false
-            val windowInsetsCompat = WindowInsetsCompat.toWindowInsetsCompat(insets)
-            return windowInsetsCompat.isVisible(WindowInsetsCompat.Type.navigationBars())
+            return insets.isVisible(WindowInsets.Type.navigationBars())
         } else {
             return false
         }
